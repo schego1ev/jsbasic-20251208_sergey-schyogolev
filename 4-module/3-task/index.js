c3bf3197c8e521977ef077f57bc61beb4197a283
schego1ev <schegolev.s.n@gmail.com>
2025-12-23 16:51:54 +0300

    completed all tasks in module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,16 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.rows;
+  for (const elem of rows) {
+    if (!elem.lastElementChild.hasAttribute('data-available')) {
+      elem.setAttribute('hidden', 'true');
+    }
+    if (elem.childNodes[5].textContent === 'm') { elem.classList.add('male'); }
+    if (elem.childNodes[5].textContent === 'f') { elem.classList.add('female'); }
+    if (elem.childNodes[3].textContent < 18) { elem.style = "text-decoration: line-through"; }
+    if (elem.lastElementChild.getAttribute('data-available') === 'true') {
+      elem.classList.add('available');
+    } else {
+      elem.classList.add('unavailable');
+    }
+  }
 }
