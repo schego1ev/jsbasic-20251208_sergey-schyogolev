9b11b2fa5b97f3f5423133f82692f0fa508c4260
schego1ev <schegolev.s.n@gmail.com>
2026-01-03 12:07:04 +0300

    completed all tasks in module 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,4 @@
 function hideSelf() {
-  // ваш код...
+  const button = document.querySelector('.hide-self-button');
+  button.addEventListener("click", () => button.hidden = true);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  const carouselContainer = document.querySelector('.carousel__inner');
+  const carouselButtonLeft = document.querySelector('.carousel__arrow_left');
+  const carouselButtonRight = document.querySelector('.carousel__arrow_right');
+  const carouselItems = document.querySelectorAll('.carousel__slide');
+  let currentIndex = 0;
+
+  function hideArrow(index) {
+    if (index === 0) {
+      carouselButtonLeft.style.display = 'none';
+      carouselButtonRight.style.display = 'flex';
+    } else {
+      carouselButtonLeft.style.display = 'flex';
+    }
+    if (index === carouselItems.length - 1) {
+      carouselButtonRight.style.display = 'none';
+    } else {
+      carouselButtonRight.style.display = 'flex';
+    }
+  }
+
+  //Вызов для проверки состояния при загрузке страницы
+  hideArrow(currentIndex);
+
+  function offset(index) {
+    carouselContainer.style.transform = `translateX(-${carouselItems[index].offsetWidth * index}px)`;
+  }
+  
+  carouselButtonRight.addEventListener("click", () => {
+    currentIndex += 1;
+    
+    hideArrow(currentIndex);
+    offset(currentIndex);
+  });
+
+  carouselButtonLeft.addEventListener("click", () => {
+    currentIndex -= 1;
+    
+    hideArrow(currentIndex);
+    offset(currentIndex);
+  });  
 }
